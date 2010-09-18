f8a6180364b2f48bec0830176b628d24b989ce16
fabiano.bie <fabiano.bie@793d8712-3ea0-81e3-2c24-ab7f4c7e0129>
2010-09-18 18:22:14 +0000

    Implementação qse concluida

--- FFTCana/src/br/ufc/cana/fft/DoFFT.java
+++ FFTCana/src/br/ufc/cana/fft/DoFFT.java
@@ -1,6 +1,7 @@
 package br.ufc.cana.fft;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import br.ufc.cana.fft.math.Complex;
 import br.ufc.cana.fft.math.FFT;
@@ -11,18 +12,52 @@ public class DoFFT {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
 		//Entradas
-		int[] eqcA = { 1, 2 }; // 1 + 2x
-		int[] eqcB = { 3, 5 }; // 3 + 5x
-		int[] eqcC;
+		int[] A = geradorNumerosAleatorios(100,1000); //{ 1, 2 }; // 1 + 2x
+		int[] B = geradorNumerosAleatorios(100,1000);//{ 3, 5 }; // 3 + 5x
+		
+		int[] C  = executeFFT(A, B);
+		
+		geradorNumerosAleatorios(10, 100);
 
+		//Imprime resultado
+		System.out.println("\nImprime resultado");
+		for (int i = 0; i < C.length; i++) {
+			System.out.print(C[i] + ",");
+		}
+
+	}
+	
+	
+    public static int[] geradorNumerosAleatorios(int range, int tamanho) {
+		if (tamanho <= 0) {
+			return null;
+		} else {
+			int numeroSorteado;
+			int sinal;
+			Random random = new Random();
+			int[] numeros = new int[tamanho];
+
+			for (int i = 0; i < tamanho; i++) {
+				numeroSorteado = random.nextInt(range + 1);
+				sinal = (random.nextInt(range + 1) > numeroSorteado)?-1:1;
+				numeros[i] = sinal*numeroSorteado;
+			}
+
+			return numeros;
+		}
+	}
+
+    
+    public static int[] executeFFT(int[] eqcA , int eqcB[]){
 		//Inicializado variáveis
 		ArrayList<Complex> A = new ArrayList<Complex>();
 		ArrayList<Complex> B = new ArrayList<Complex>();
 		ArrayList<Complex> cmplxA = new ArrayList<Complex>();
 		ArrayList<Complex> cmplxB = new ArrayList<Complex>();
 		ArrayList<Complex> cmplxC;
+		int[] resultado;
 
 		//Duplicando o limite de grau
 		//Gerando numeros complexos
@@ -30,8 +65,8 @@ public class DoFFT {
 		B = Complex.doubleToComplex(eqcB, eqcB.length * 2);
 		
 		System.out.println("\nArrays de Entrada");
-		Complex.printComplex(A);
-		Complex.printComplex(B);
+		//Complex.printComplex(A);
+		//Complex.printComplex(B);
 		
 		//Fast Fourier Transform
 		FFT fft = new FFT();
@@ -41,26 +76,25 @@ public class DoFFT {
 		cmplxA = fft.FFTrecursivo(A, false);
 		cmplxB = fft.FFTrecursivo(B, false);
 		
-		Complex.printComplex(cmplxA);
-		Complex.printComplex(cmplxB);
+		//Complex.printComplex(cmplxA);
+		//Complex.printComplex(cmplxB);
 		
 		
 		//Multiplicar
 		System.out.println("\nMultiplicar");
 		cmplxC = fft.multiplicacao(cmplxA, cmplxB);
 		
-		Complex.printComplex(cmplxC);
+		//Complex.printComplex(cmplxC);
 		
 		//Interpolar
 		System.out.println("\nInterpolar");
-		eqcC = fft.FFTinverso(cmplxC);
-
-		//Imprime resultado
-		System.out.println("\nImprime resultado");
-		for (int i = 0; i < eqcC.length; i++) {
-			System.out.print(eqcC[i] + ",");
-		}
-
-	}
+		resultado = fft.FFTinverso(cmplxC);
+		
+		return resultado;
+    }
+    
+	
+	
+	
 
 }
