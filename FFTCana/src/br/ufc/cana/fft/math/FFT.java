8d8bee0d96685d66853a7060083a248f37045519
pauloalr <pauloalr@793d8712-3ea0-81e3-2c24-ab7f4c7e0129>
2010-09-18 15:20:47 +0000

    fft recursivo

--- FFTCana/src/br/ufc/cana/fft/math/FFT.java
+++ FFTCana/src/br/ufc/cana/fft/math/FFT.java
@@ -4,42 +4,68 @@ import java.util.ArrayList;
 
 public class FFT {
 
-	public ArrayList<Complex> multiplicacao(ArrayList<Complex> A, ArrayList<Complex> B) {
-		
+	public ArrayList<Complex> multiplicacao(ArrayList<Complex> A,
+			ArrayList<Complex> B) {
+
 		ArrayList<Complex> C = new ArrayList<Complex>();
-		
+
 		for (int i = 0; i < A.size(); i++) {
 			C.add(B.get(i).times(A.get(i)));
 		}
-		
+
 		return C;
 	}
-	
-	private ArrayList<Complex> impar(ArrayList<Complex> V){
-		
+
+	public static ArrayList<Complex> impar(ArrayList<Complex> V) {
+
 		ArrayList<Complex> imparList = new ArrayList<Complex>();
-		int tamImpar = 0 ;
-		int n =  V.size();
-		
-		tamImpar = ((n % 2) == 1)?(n-1)/2 : n/2;
-		
+		int tamImpar = 0;
+		int n = V.size();
+
+		tamImpar = ((n % 2) == 1) ? (n - 1) / 2 : n / 2;
+
 		for (int i = 0; i < tamImpar; i++) {
-			imparList.add(V.get(2*i+1));
+			imparList.add(V.get(2 * i + 1));
 		}
 		return imparList;
 	}
-	
-	private ArrayList<Complex> par(ArrayList<Complex> V){
-		
+
+	public static ArrayList<Complex> par(ArrayList<Complex> V) {
+
 		ArrayList<Complex> parList = new ArrayList<Complex>();
-		int tamPar = 0 ;
-		int n =  V.size();
-		
-		tamPar = ((n % 2) == 1)?(n+1)/2 : n/2;
-		
+		int tamPar = 0;
+		int n = V.size();
+
+		tamPar = ((n % 2) == 1) ? (n + 1) / 2 : n / 2;
+
 		for (int i = 0; i < tamPar; i++) {
-			parList.add(V.get(2*i));
+			parList.add(V.get(2 * i));
 		}
 		return parList;
+	}
+
+	public static ArrayList<Complex> FFTrecursivo(ArrayList<Complex> A, int m,
+			Complex c) {
+		ArrayList<Complex> V = new ArrayList<Complex>();
+		if (m == 1) {
+			V.add(0, A.get(0));
+			return V;
+		}
+
+		ArrayList<Complex> Ap = FFT.par(A);
+		ArrayList<Complex> Ai = FFT.impar(A);
+
+		ArrayList<Complex> Vp = FFTrecursivo(Ap, Ap.size(), c.pow(2));
+		ArrayList<Complex> Vi = FFTrecursivo(Ai, Ai.size(), c.pow(2));
+
+		for (int i = 0; i < m; i++) {
+			V.add(i, new Complex(0, 0));
+		}
+
+		for (int i = 0; i < m / 2; i++) {
+			V.set(i, Vp.get(i).plus(Vi.get(i).times(c.pow(i))));
+			V.set(i + (m / 2), Vp.get(i).minus(Vi.get(i).times(c.pow(i))));
+		}
+		return V;
 	}
 }
