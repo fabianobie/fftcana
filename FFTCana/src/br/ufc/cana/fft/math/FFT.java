cf3c7c8ccce7a1d2c6937fcccecae5c1554bafa6
fabiano.bie <fabiano.bie@793d8712-3ea0-81e3-2c24-ab7f4c7e0129>
2010-09-18 14:30:12 +0000

    coeficientes Impar e Par de um ArrayList

--- FFTCana/src/br/ufc/cana/fft/math/FFT.java
+++ FFTCana/src/br/ufc/cana/fft/math/FFT.java
@@ -1,5 +1,36 @@
 package br.ufc.cana.fft.math;
 
+import java.util.ArrayList;
+
 public class FFT {
 
+	private ArrayList<Complex> impar(ArrayList<Complex> V){
+		
+		ArrayList<Complex> imparList = new ArrayList<Complex>();
+		int tamImpar = 0 ;
+		int n =  V.size();
+		
+		tamImpar = ((n % 2) == 1)?(n-1)/2 : n/2;
+		
+		for (int i = 0; i < tamImpar; i++) {
+			imparList.add(V.get(2*i+1));
+		}
+		return imparList;
+	}
+	
+	private ArrayList<Complex> par(ArrayList<Complex> V){
+		
+		ArrayList<Complex> parList = new ArrayList<Complex>();
+		int evenTamanho = 0 ;
+		int n =  V.size();
+		
+		evenTamanho = ((n % 2) == 1)?(n+1)/2 : n/2;
+		
+		for (int i = 0; i < evenTamanho; i++) {
+			parList.add(V.get(2*i));
+		}
+		return parList;
+	}
+	
+	
 }
